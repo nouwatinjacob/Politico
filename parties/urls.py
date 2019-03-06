558ab5caddf6eea7deb1290605af866f255de6a1
Jacob <jaysansa@gmail.com>
2019-03-06 15:38:08 +0100

    feat(craete-party): An endpoint to create a party
    - add view to create a new party
    - add url for partyList
    
    [Finishes #164094252]

--- parties/urls.py
+++ parties/urls.py
@@ -1,5 +1,7 @@
-
+from django.urls import path
+from .views import PartyList
 
 urlpatterns = [
-    
+    path("parties/", PartyList.as_view(), name="parties_create"),
+    # path("parties/<int:pk>", PartyDetail.as_view(), name="parties_detail")
 ]
--- politico/settings.py
+++ politico/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Set the url to work even without slash
+APPEND_SLASH=True
