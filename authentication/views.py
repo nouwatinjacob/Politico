a6ca62238966c2d203bd0872f9a98a0e7a33eb63
Jacob <jaysansa@gmail.com>
2019-03-22 14:43:34 +0100

    chore(user-model): update user model
    - change the user authentication to use jwt
    - modify the login details to use email
    - include token to the ressponse after user signup
    
    [Finishes #164787637]

--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 from rest_framework.views import APIView
 from rest_framework import generics
 from django.http import Http404
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework_jwt.settings import api_settings
+
+from rest_framework import permissions
 
 from django.contrib.auth.models import User
 
-from .serializers import UserSerializer
+from .serializers import UserSerializerWithToken, TokenSerializer
+
+
+# Get the JWT settings, add these lines after the import/from lines
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 # Create your views here.
 
@@ -17,12 +25,12 @@ class UserCreate(APIView):
     authentication_classes = ()
     permission_classes = ()
     def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
+        serializer = UserSerializerWithToken(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({
                 "status": 201,
-                "data": [serializer.data]
+                "data": [serializer.data],
                 }, status=status.HTTP_201_CREATED)
         return Response({
             "status":400, 
@@ -30,17 +38,22 @@ class UserCreate(APIView):
             }, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(APIView):
-    permission_classes = ()
+    permission_classes = (permissions.AllowAny,)
     def post(self, request, format=None):
-        username = request.data.get("username")
+        email = request.data.get("email")
         password = request.data.get("password")
-        user = authenticate(username=username, password=password)
-        if user:
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            serializer = TokenSerializer(data={
+                # using drf jwt utility functions to generate a token
+                "token": jwt_encode_handler(
+                    jwt_payload_handler(user)
+                )})
+            serializer.is_valid()
             return Response({
                 "status": 200,
-                "data": {
-                    "token": user.auth_token.key
-                }
+                "data": serializer.data
             }, status=status.HTTP_200_OK)
         else:
             return Response({
