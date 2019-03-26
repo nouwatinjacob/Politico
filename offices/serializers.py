4269bb31eb159607e494b72e2f031e651ca5c692
Jacob <jaysansa@gmail.com>
2019-03-26 15:41:51 +0100

    feat(candidate): An endpoint to register a user for a politico office
    - create candidate model
    - create candidate serializers
    - add view to add register the user to be a candidate
    
    [Finishes #164850627]

--- offices/serializers.py
+++ offices/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
-from .models import Office
+from django.utils.translation import ugettext_lazy as _
+
+from .models import OfficeModel, Candidate
 
 class OfficeSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Office
+        model = OfficeModel
         fields = '__all__'
+        extra_kwargs = {
+            "name": {"required": True, }
+        }
+
+class CandidateSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.id')
+    class Meta:
+        model = Candidate
+        fields = ('office', 'user')
+        
--- offices/urls.py
+++ offices/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import OfficeList, OfficeDetail
+from .views import OfficeList, OfficeDetail, CandidateView
 
 urlpatterns = [
     path("offices/", OfficeList.as_view(), name="office_create"),
-    path("offices/<int:pk>", OfficeDetail.as_view(), name="offices_details")
+    path("offices/<int:pk>", OfficeDetail.as_view(), name="offices_details"),
+    path("office/register", CandidateView.as_view(), name="register_candidate")
 ]
