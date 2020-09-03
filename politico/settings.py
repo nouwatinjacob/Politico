208262cef9403aede056c08ddf84bd83d4743192
Jacob Nouwatin <jaysansa@gmail.com>
2020-09-03 01:30:21 +0100

    little changes to the settings

--- politico/settings.py
+++ politico/settings.py
@@ -132,10 +132,10 @@ WSGI_APPLICATION = 'politico.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'politico',                      
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('DB_NAME'),                      
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': '5432',
     }
 }
